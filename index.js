94445c047b7dc5a2ed8d9ac5da25a5a11ce1a4c8
Ererej <erikeliassonjohansson@gmail.com>
2024-05-22 10:56:13 +0200

    saving works and loading almost works. exept for that it does not get rid off all the existing seats

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let life = 52;
 const itemspawners = document.getElementsByClassName("item-spawner")
 const drawingArea = document.getElementById("drawing-area")
 const rows = 5
@@ -6,7 +7,77 @@ const gridheight = 65/rows
 const gridWidth = 78/columns
 const grid = []
 
-getCookie
+// taken from stakoverflow
+function setCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+// taken from w3schools
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+const addLoadButton = (saveName) => {
+    const dropDown = document.getElementById("saves-dropdown")
+    const element = document.createElement("button")
+    element.classList.add("saves-dropdown-button")
+    element.classList.add("saves-dropdown-content")
+
+    element.innerHTML = saveName
+    dropDown.appendChild(element)
+    element.addEventListener("click", function() {
+        const grid = JSON.parse(getCookie("seatingPlans"))[saveName]
+        for (let i=0; i<9; i++) {
+            for (let j=0; j<5; j++) {
+                    drawingArea.childNodes.forEach(async (element) => {
+                        if (element.style.gridColumnStart == i+1 && element.style.gridRowStart == j+1) {
+                            element.style.backgroundColor = grid[i][j][0].color
+                            element.innerHTML = grid[i][j][1]
+                            element.classList.add("seat")
+                        }
+                    })
+            }
+        }
+    })
+}
+
+let saveNames = ""
+try {
+    if (getCookie("seatingPlans") != "") {
+        saveNames = JSON.parse(getCookie("seatingPlans"))
+    }
+} catch (error) {
+    console.log("no saves")
+}
+if (getCookie("seatingPlans") != "") {
+    saveNames = JSON.parse(getCookie("seatingPlans"))
+}
+if (saveNames && typeof saveNames === "object" && Object.keys(saveNames).length > 0){
+    saveNames = Object.keys(saveNames)
+    saveNames.forEach(async (name) => {
+        addLoadButton(name)
+    })
+}
 
 for (let i=0; i<9; i++) {
     grid.push([])
@@ -58,7 +129,7 @@ drawingArea.addEventListener("click", function(event) {
         const ghostItemBox = ghostItem.getBoundingClientRect()
         const drawingAreaBox = drawingArea.getBoundingClientRect()
         if (grid[Math.floor((event.clientX-drawingAreaBox.left)/ghostItemBox.width)][Math.floor((event.clientY-drawingAreaBox.top)/ghostItemBox.height)] != null) {
-            grid[Math.floor((event.clientX-drawingAreaBox.left)/ghostItemBox.width)][Math.floor((event.clientY-drawingAreaBox.top)/ghostItemBox.height)][0] = ghostItem.id
+            grid[Math.floor((event.clientX-drawingAreaBox.left)/ghostItemBox.width)][Math.floor((event.clientY-drawingAreaBox.top)/ghostItemBox.height)][0] = {color: ghostItem.style.backgroundColor}
             console.table(grid)
             drawingArea.childNodes.forEach(async (element) => {
                 if (element.style.gridColumnStart == Math.floor((event.clientX-drawingAreaBox.left)/ghostItemBox.width + 1) && element.style.gridRowStart == Math.floor((event.clientY-drawingAreaBox.top)/ghostItemBox.height + 1)) {
@@ -87,70 +158,38 @@ document.addEventListener("mousemove", function(event) {
 
 document.getElementById("saveButton").addEventListener("click", function() {
     const name = prompt("give the classroom a name")
+    if (name == null || name.length <= 0) return
+
+    console.log(name)
     if (getCookie("seatingPlans") == "") {
-        setCookie("seatingPlans", JSON.stringify([{[name]: grid}]))
+        setCookie("seatingPlans", JSON.stringify({[name]: grid}))
     }
-    setCookie("seatingPlans", JSON.stringify(JSON.parse(getCookie("seatingPlans")).push({[name]: grid})))
+    const seatingPlans = JSON.parse(getCookie("seatingPlans"))
+    seatingPlans[name] = grid
+    setCookie("seatingPlans", JSON.stringify(seatingPlans))
+    addLoadButton(name)
+    console.log(JSON.parse(getCookie("seatingPlans")))
 })
 
 function showSaves() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+    const content = document.getElementsByClassName("saves-dropdown-content")
+    for (let i=0; i<content.length; i++) {
+        content[i].classList.toggle("show")
+    }
+}
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
+    if (!event.target.matches('#loadButton')) {
+        var dropdowns = document.getElementsByClassName("saves-dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
-            }
+                console.log("hiding")
+            } 
         }
     }
 }
 
-document.getElementById("loadButton").addEventListener("click", function() {
-    const grid = JSON.parse(getCookie("seating "))
-    for (let i=0; i<9; i++) {
-        for (let j=0; j<5; j++) {
-            if (grid[i][j][0] != null) {
-                drawingArea.childNodes.forEach(async (element) => {
-                    if (element.style.gridColumnStart == i+1 && element.style.gridRowStart == j+1) {
-                        element.style.backgroundColor = grid[i][j][0]
-                        element.innerHTML = grid[i][j][0]
-                    }
-                })
-            }
-        }
-    }
-})
 
-// taken from stakoverflow
-function setCookie(name, value, days) {
-    var expires;
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    }
-    else {
-        expires = "";
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-// taken from stakoverflow
-function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-        c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) {
-                c_end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return "";
-}
